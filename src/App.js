496b5d4f5b53fba167fc80f1bd2a7b80c0de982d
christian conti <christian.conti123@gmail.com>
2024-01-23 13:04:26 +0100

    eroe

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className='filter'></div>
+        <div className="navbar-b container-fluid">
+          <div className="row w-100 h-100">
+          <div className="position-relative d-flex justify-content-center align-items-center col-6">
+            <div className=' overflow-hidden'>
+            <p className="text_logo text">BURNINGATE</p>
+            </div>
+            </div>
+          <div className="col-6">
+            <div className="row h-100 w-100 justify-content-center align-items-center">
+            <div className='col-3 h-100'><button className='text nav_link'>home</button></div>
+              <div className='col-3 h-100'><button className='text nav_link'>about-us</button></div>
+              <div className='col-3 h-100'><button className='text nav_link'>course</button></div>
+              <div className='col-3 h-100'><button className='text nav_link'>campionship</button></div>
+            </div>
+          </div>
+          </div>
+        </div>
+        <div className="title_block">
+        <h1 className="text title">BURNINGATE</h1>
+        <h2 className="text subtitle">DEVELOP YOUR STRENGTH</h2>
+        </div>
       </header>
     </div>
   );
