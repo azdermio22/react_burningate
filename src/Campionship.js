ad75413550b624c52d1e0d7f421656e4d18792e0
christian conti <christian.conti123@gmail.com>
2024-02-01 16:47:59 +0100

    campionship

--- src/Campionship.js
+++ src/Campionship.js
@@ -6,11 +6,47 @@ export const Campionship = () => {
         let left_slide = document.querySelector('.left_slide');
         let right_slide = document.querySelector('.right_slide');
         let center_slide = document.querySelector('.center_slide');
+        let right_filter = document.querySelector('.right_filter');
+        let left_filter = document.querySelector('.left_filter');
+        let center_filter = document.querySelector('.center_filter');
         let back_slide = document.querySelector('.back_slide');
-        // left_slide.style.animationName = "left_slide";
-        // right_slide.style.animationName = "right_slide";
-        // center_slide.style.animationName = "center_slide";
-        // back_slide.style.animationName = "back_slide";
+
+        right_filter.addEventListener('click',()=>{
+            left_slide.style.animationName = "left_slide";
+            right_slide.style.animationName = "right_slide";
+            center_slide.style.animationName = "center_slide";
+            center_filter.style.display = "flex";
+            back_slide.style.animationName = "back_slide";
+            center_filter.style.animationName = "center_filter";
+            right_filter.style.animationName = "right_filter";
+            setTimeout(() => {
+                left_slide.style.animationName = "";
+                right_slide.style.animationName = "";
+                center_slide.style.animationName = "";
+                back_slide.style.animationName = "";
+                center_filter.style.animationName = "";
+                right_filter.style.animationName = "";
+                center_filter.style.display = "none";
+            }, 1000);
+        })
+        left_filter.addEventListener('click',()=>{
+            left_slide.style.animationName = "left_slide_left";
+            right_slide.style.animationName = "right_slide_left";
+            center_slide.style.animationName = "center_slide_left";
+            back_slide.style.animationName = "back_slide_left";
+            center_filter.style.animationName = "center_filter";
+            center_filter.style.display = "flex";
+            left_filter.style.animationName = "left_filter";
+            setTimeout(()=>{
+                left_slide.style.animationName = "";
+                right_slide.style.animationName = "";
+                center_slide.style.animationName = "";
+                back_slide.style.animationName = "";
+                center_filter.style.animationName = "";
+                left_filter.style.animationName = "";
+                center_filter.style.display = "none";
+            },1000)
+        })
     },[]);
     return (
         <div>
@@ -27,38 +63,44 @@ export const Campionship = () => {
                 </div>
             </div>
             </header>
-            <section>
+            <section className="my-5">
                 <div><p>UPCOMING COMPETITIONS</p></div>
-                <div className="d-flex justify-content-center align-items-center w-100 camp_title_container position-relative">
-
-                <div className="bg-danger text-center d-flex flex-column align-items-center tournament scale_side_slide left_slide">
-                <div className="camp_competition scale_text mt-5">CALISTHENICS OFFICIAL CIRCUIT & MILAN</div>
-                <div className="camp_competition scale_text">25/12/2024</div>
-                <button className="title_button">MORE INFO</button>
-                <div className="logo_white"></div>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-4 p-0">
+                        <div className="competition bg-danger text-center pt-3 scale_side_slide left_slide">
+                        <div className="text-white fs-3">CALISTHENICS OFFICIAL CIRCUIT & MILAN</div>
+                        <div className="text-white fs-3">25/12/2024</div>
+                        <button className="competition_button">MORE INFO</button>
+                        <div className="left_filter"></div>
+                            <div className="logo_white"></div>
+                        </div>
+                    </div>
+                    <div className="col-4 p-0 position-relative">
+                    <div className="competition bg-danger text-center pt-3 center_slide">
+                    <div className="text-white fs-3">CALISTHENICS OFFICIAL CIRCUIT & MILAN</div>
+                        <div className="text-white fs-3">25/12/2024</div>
+                        <button className="competition_button">MORE INFO</button>
+                        <div className="center_filter"></div>
+                    <div className="logo_white"></div>
+                    </div>
+                    <div className="competition bg-danger text-center pt-3 position-absolute back_slide">
+                    <div className="text-white fs-3">CALISTHENICS OFFICIAL CIRCUIT & MILAN</div>
+                        <div className="text-white fs-3">25/12/2024</div>
+                        <button className="competition_button">MORE INFO</button>
+                    <div className="logo_white"></div>
+                    </div>
+                    </div>
+                    <div className="col-4 p-0">
+                    <div className="competition bg-danger text-center pt-3 scale_side_slide right_slide">
+                    <div className="text-white fs-3">CALISTHENICS OFFICIAL CIRCUIT & MILAN</div>
+                        <div className="text-white fs-3">25/12/2024</div>
+                        <button className="competition_button">MORE INFO</button>
+                        <div className="right_filter"></div>
+                    <div className="logo_white"></div>
+                    </div>
+                    </div>
                 </div>
-            
-                <div className="bg-danger text-center d-flex flex-column align-items-center tournament center_slide">
-                <div className="camp_competition scale_text mt-5">CALISTHENICS OFFICIAL CIRCUIT & MILAN</div>
-                <div className="camp_competition scale_text">25/12/2024</div>
-                <button className="title_button">MORE INFO</button>
-                <div className="logo_white"></div>
-                </div>
-
-                <div className="bg-danger text-center d-flex flex-column align-items-center tournament back_slide position-absolute">
-                <div className="camp_competition scale_text mt-5">CALISTHENICS OFFICIAL CIRCUIT & MILAN</div>
-                <div className="camp_competition scale_text">25/12/2024</div>
-                <button className="title_button">MORE INFO</button>
-                <div className="logo_white"></div>
-                </div>
-
-                <div className="bg-danger text-center d-flex flex-column align-items-center tournament scale_side_slide right_slide">
-                <div className="camp_competition scale_text  mt-5">CALISTHENICS OFFICIAL CIRCUIT & MILAN</div>
-                <div className="camp_competition scale_text">25/12/2024</div>
-                <button className="title_button">MORE INFO</button>
-                <div className="logo_white"></div>
-                </div>
- 
             </div>
             </section>
 
@@ -68,7 +110,7 @@ export const Campionship = () => {
     <div>
         <div className="row">
             <div className="col-6 p-0"><p>eifu ueb i vt v crs crcatdc crc ftrxtyjhsdbaic uuy iui uf es rfres wesdt v ug es ex sazwxw zsdxeszxty nkljn jvdtzaw zerx zaw zwe z3w 3 ewstrd err ex rxrx  rsdx drxc dr x rszes zesxsxsexzse zsez ez</p></div>
-            <div className="col-6 p-0"><iframe width="75%" height="315" src="https://www.youtube.com/embed/81ucmcSAxzA?si=wNPGod_q8yb7S5zG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
+            <div className="col-6 p-0"><iframe width="75%" height="315" src="https://www.youtube.com/embed/81ucmcSAxzA?si=wNPGod_q8yb7S5zG" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe></div>
         </div>
     </div>
 </div>
