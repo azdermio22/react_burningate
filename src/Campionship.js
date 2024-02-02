0f357c478a7d9c6afb3604a6ba79f30fc3e12566
christian conti <christian.conti123@gmail.com>
2024-02-02 11:25:54 +0100

    campionship

--- src/Campionship.js
+++ src/Campionship.js
@@ -14,6 +14,8 @@ export const Campionship = () => {
         right_filter.addEventListener('click',()=>{
             left_slide.style.animationName = "left_slide";
             right_slide.style.animationName = "right_slide";
+            right_slide.style.zIndex = "1";
+            left_slide.style.zIndex = "0";
             center_slide.style.animationName = "center_slide";
             center_filter.style.display = "flex";
             back_slide.style.animationName = "back_slide";
@@ -31,6 +33,8 @@ export const Campionship = () => {
         })
         left_filter.addEventListener('click',()=>{
             left_slide.style.animationName = "left_slide_left";
+            left_slide.style.zIndex = "1";
+            right_slide.style.zIndex = "0";
             right_slide.style.animationName = "right_slide_left";
             center_slide.style.animationName = "center_slide_left";
             back_slide.style.animationName = "back_slide_left";
@@ -67,8 +71,8 @@ export const Campionship = () => {
                 <div><p>UPCOMING COMPETITIONS</p></div>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-4 p-0">
-                        <div className="competition bg-danger text-center pt-3 scale_side_slide left_slide">
+                    <div className="col-4 p-0 position-relative">
+                        <div className="competition bg-danger text-center pt-3 scale_side_slide position-absolute left_slide">
                         <div className="text-white fs-3">CALISTHENICS OFFICIAL CIRCUIT & MILAN</div>
                         <div className="text-white fs-3">25/12/2024</div>
                         <button className="competition_button">MORE INFO</button>
@@ -77,14 +81,14 @@ export const Campionship = () => {
                         </div>
                     </div>
                     <div className="col-4 p-0 position-relative">
-                    <div className="competition bg-danger text-center pt-3 center_slide">
+                    <div className="competition bg-danger text-center pt-3 center_slide z-1">
                     <div className="text-white fs-3">CALISTHENICS OFFICIAL CIRCUIT & MILAN</div>
                         <div className="text-white fs-3">25/12/2024</div>
                         <button className="competition_button">MORE INFO</button>
                         <div className="center_filter"></div>
                     <div className="logo_white"></div>
                     </div>
-                    <div className="competition bg-danger text-center pt-3 position-absolute back_slide">
+                    <div className="competition bg-danger text-center pt-3 position-absolute back_slide z-0">
                     <div className="text-white fs-3">CALISTHENICS OFFICIAL CIRCUIT & MILAN</div>
                         <div className="text-white fs-3">25/12/2024</div>
                         <button className="competition_button">MORE INFO</button>
@@ -104,13 +108,20 @@ export const Campionship = () => {
             </div>
             </section>
 
-            <section>               
+            <section className="p-0">               
 <div><p>PAST COMPETITION</p></div>
 <div>
-    <div>
+    <div className="past_competition bg-dark position-relative">
         <div className="row">
-            <div className="col-6 p-0"><p>eifu ueb i vt v crs crcatdc crc ftrxtyjhsdbaic uuy iui uf es rfres wesdt v ug es ex sazwxw zsdxeszxty nkljn jvdtzaw zerx zaw zwe z3w 3 ewstrd err ex rxrx  rsdx drxc dr x rszes zesxsxsexzse zsez ez</p></div>
-            <div className="col-6 p-0"><iframe width="75%" height="315" src="https://www.youtube.com/embed/81ucmcSAxzA?si=wNPGod_q8yb7S5zG" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe></div>
+            <div className="col-4 d-flex justify-content-center align-items-center">
+                <div className="competition bg-danger text-center">
+                <div className="text-white fs-3">CALISTHENICS OFFICIAL CIRCUIT & MILAN</div>
+                        <div className="text-white fs-3">25/12/2024</div>
+                        <div className="text-white fs-5 mt-4">2 SET RANKING CLASSIC 75” + 60” </div>
+                </div>
+            </div>
+            <div className="col-4 p-0 text-white"><p>eifu ueb i vt v crs crcatdc crc ftrxtyjhsdbaic uuy iui uf es rfres wesdt v ug es ex sazwxw zsdxeszxty nkljn jvdtzaw zerx zaw zwe z3w 3 ewstrd err ex rxrx  rsdx drxc dr x rszes zesxsxsexzse zsez ez</p></div>
+            <div className="col-4 p-0"><iframe width="75%" height="315" src="https://www.youtube.com/embed/81ucmcSAxzA?si=wNPGod_q8yb7S5zG" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe></div>
         </div>
     </div>
 </div>
